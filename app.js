1bef5d1228471db092fda683cc56fbfaa1c3c478
M00845272 <SL1558@live.mdx.ac.uk>
2024-01-24 20:56:54 +0000

    order API implementation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(bodyParser.json());
 
 let lessons = [
-      {
+    {
         "id": 1001,
         "subject": "Math",
         "location": "London",
@@ -14,8 +14,8 @@ let lessons = [
         "image": "assets/images/math_icon_256px.png",
         "availableInventory": 5,
         "rating": 1
-      },
-      {
+    },
+    {
         "id": 1002,
         "subject": "Math",
         "location": "Oxford",
@@ -23,8 +23,8 @@ let lessons = [
         "image": "assets/images/math_icon_256px.png",
         "availableInventory": 10,
         "rating": 3
-      },
-      {
+    },
+    {
         "id": 1003,
         "subject": "Science",
         "location": "London",
@@ -32,8 +32,8 @@ let lessons = [
         "image": "assets/images/science_icon_256px.png",
         "availableInventory": 5,
         "rating": 3
-      },
-      {
+    },
+    {
         "id": 1004,
         "subject": "Science",
         "location": "Oxford",
@@ -41,8 +41,8 @@ let lessons = [
         "image": "assets/images/science_icon_256px.png",
         "availableInventory": 10,
         "rating": 4
-      },
-      {
+    },
+    {
         "id": 1005,
         "subject": "Music",
         "location": "London",
@@ -50,8 +50,8 @@ let lessons = [
         "image": "assets/images/music_icon_256px.png",
         "availableInventory": 5,
         "rating": 2
-      },
-      {
+    },
+    {
         "id": 1006,
         "subject": "Music",
         "location": "Oxford",
@@ -59,8 +59,8 @@ let lessons = [
         "image": "assets/images/music_icon_256px.png",
         "availableInventory": 10,
         "rating": 5
-      },
-      {
+    },
+    {
         "id": 1007,
         "subject": "English",
         "location": "London",
@@ -68,8 +68,8 @@ let lessons = [
         "image": "assets/images/english_icon_256px.png",
         "availableInventory": 5,
         "rating": 2
-      },
-      {
+    },
+    {
         "id": 1008,
         "subject": "English",
         "location": "Oxford",
@@ -77,8 +77,8 @@ let lessons = [
         "image": "assets/images/english_icon_256px.png",
         "availableInventory": 10,
         "rating": 3
-      },
-      {
+    },
+    {
         "id": 1009,
         "subject": "English",
         "location": "York",
@@ -86,8 +86,8 @@ let lessons = [
         "image": "assets/images/english_icon_256px.png",
         "availableInventory": 5,
         "rating": 3
-      },
-      {
+    },
+    {
         "id": 1010,
         "subject": "Math",
         "location": "York",
@@ -95,8 +95,8 @@ let lessons = [
         "image": "assets/images/math_icon_256px.png",
         "availableInventory": 5,
         "rating": 4
-      },
-      {
+    },
+    {
         "id": 1011,
         "subject": "Science",
         "location": "York",
@@ -104,8 +104,8 @@ let lessons = [
         "image": "assets/images/science_icon_256px.png",
         "availableInventory": 5,
         "rating": 5
-      },
-      {
+    },
+    {
         "id": 1012,
         "subject": "Music",
         "location": "York",
@@ -113,8 +113,8 @@ let lessons = [
         "image": "assets/images/music_icon_256px.png",
         "availableInventory": 5,
         "rating": 5
-      }
-    ];
+    }
+];
 
 // Get list of lessons
 app.get('/lessons', (req, res) => {
@@ -126,31 +126,42 @@ app.get('/lessons', (req, res) => {
 // API always reduces available spaces by 1
 app.put('/lesson/update_availability/:id', (req, res) => {
     const lessonId = parseInt(req.params.id);
-    console.log('update_availability API called. LessonId='+lessonId);
+    console.log('update_availability API called. LessonId=' + lessonId);
 
     // Find the lesson by ID
     const index = lessons.findIndex((lesson) => lesson.id === lessonId);
-    console.log('Available spaces before update: ', lessons[index] );
+    console.log('Available spaces before update: ', lessons[index]);
 
-    let currentAvailableSPaces = lessons[index]['availableInventory'] ;
+    let currentAvailableSPaces = lessons[index]['availableInventory'];
 
     // return error if not enough available spaces
-    if(currentAvailableSPaces < 1){
+    if (currentAvailableSPaces < 1) {
         res.status(404).json({ message: 'Can\'t update, not enough available spaces.' });
         return;
     }
-  
+
 
     if (index !== -1) {
-      // Update the lesson available count
-      lessons[index]['availableInventory'] = currentAvailableSPaces - 1;
-      console.log('Available spaces updated successfully for', lessons[index] );
+        // Update the lesson available count
+        lessons[index]['availableInventory'] = currentAvailableSPaces - 1;
+        console.log('Available spaces updated successfully for', lessons[index]);
 
-      res.json({ message: 'Available spaces after successfull update: ', lesson: lessons[index] });
+        res.json({ message: 'Available spaces after successfull update: ', lesson: lessons[index] });
     } else {
-      res.status(404).json({ message: 'Lesson not found' });
+        res.status(404).json({ message: 'Lesson not found' });
     }
-  });
+});
+
+let orders = [];
+// Create an order
+app.post('/order', (req, res) => {
+    const orderDetails = req.body;
+    console.log('order API called. OrderDetails:' + JSON.stringify(orderDetails));
+
+    // Add the order to the orders array
+    orders.push(orderDetails);
+    res.json({ message: 'Order created successfully', order: orderDetails });
+});
 
 
 http.createServer(app).listen(3000); // start the server
